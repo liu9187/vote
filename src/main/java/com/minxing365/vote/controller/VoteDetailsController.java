238b72c0cc8719099395ae44d7a3a254bab12480
liucl <liucl@dehuinet.com>
2018-11-28 15:24:56 +0800

    增加VoteDetails查询日志

--- src/main/java/com/minxing365/vote/controller/VoteDetailsController.java
+++ src/main/java/com/minxing365/vote/controller/VoteDetailsController.java
@@ -5,6 +5,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,19 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v2/vote")
 @Api("投票详情api文档")
 public class VoteDetailsController {
+    Logger log = LoggerFactory.getLogger(VoteDetailsController.class);
     @Autowired
     VotingDetailsService votingDetailsService;
 
     /**
-     * 根据id查询数据
+     * 根据name查询数据
      * @param name 投票主题
      * @return
      */
     @ApiOperation("根据id查询数据")
     @ApiImplicitParams(@ApiImplicitParam(paramType = "query",name = "name",dataType = "String",required = true,value = "投票主题"))
-    @RequestMapping(value = "/selectVotingDetailsById", method = {RequestMethod.GET})
-    public String selectVotingDetailsById(@RequestParam String name) {
-
-        return votingDetailsService.selectVotingDetailsByName(name);
+    @RequestMapping(value = "/selectVotingDetailsByName", method = {RequestMethod.GET})
+    public String selectVotingDetailsByName(@RequestParam String name) {
+          log.info("------根据投票主题查询相关数据 参数：name="+name);
+          String result=null;
+          try {
+             result= votingDetailsService.selectVotingDetailsByName(name);
+          }catch (Exception e){
+               log.error("<<<<<<<selectVotingDetailsByName调用出现错误",e);
+          }
+        return result;
     }
 }
